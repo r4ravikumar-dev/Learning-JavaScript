aee424b3badfb27704591f32398b82441df61250
Ravi Kumar <62147216+r4ravikumar-dev@users.noreply.github.com>
2023-08-04 19:16:48 +0000

    variables and constants

---
+++ basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "r4avikumar@google.com"
+var accoundPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+console.log("accountId");
+console.table([accountId, accountEmail, accoundPassword, accountCity, accountState])
