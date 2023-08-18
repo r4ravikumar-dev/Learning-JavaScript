8d7d71a3cb45c5a4c0c7cd2d2f8aad06cc43047e
Ravi Kumar <62147216+r4ravikumar-dev@users.noreply.github.com>
2023-08-18 18:13:19 +0000

    learned about datatype comparison and conversion methods

---
+++ basics/comparison.js
@@ -0,0 +1,27 @@
+console.log(2>1)
+console.log(2>=1)
+console.log(2<1)
+console.log(2==1)
+console.log(2!=1)
+
+
+console.log("2">1);
+console.log("02">1);
+
+
+console.log(null>0);
+console.log(null==0); //equality check operator
+console.log(null>=0); //here null is convetred into 0 and then comparison takes place ( >= comparison operator)
+
+//the reason is that an equality check (==) and comparison (<,>.<=,>=) works differently 
+//comparison converts null to  an number testing it as 0, thats why null>=0 is true and null==0 is false\
+
+
+console.log (undefined ==0);
+console.log (undefined <0);
+console.log (undefined >=0);
+//for undefined equality check and comparison gives same result and gives false as result
+
+//******************** Strict check(===)*********************
+//check the value strictly and checks the datatypes
+console.log ("02"===1);
