cce465c2731288d9486d6e957e052ad7f620491c
Ravi Kumar <62147216+r4ravikumar-dev@users.noreply.github.com>
2023-08-18 11:11:45 +0000

    discussed conversion of datatypes

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -1,4 +1,25 @@
-let score = 33
+let score = "33"
 
-console.log(typeof score);
+console.log(typeof score); 
 console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false =>0
+
+// Boolean conversion
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log (booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "ravi" => true
+
+let somenumber = 33
+let stringnumber = String (somenumber)
+console.log(stringnumber);
+console.log (typeof stringnumber);
