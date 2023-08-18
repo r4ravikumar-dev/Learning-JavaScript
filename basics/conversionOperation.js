3792ad498ce5eee60ef238dc544fc19b9ad09c73
Ravi Kumar <62147216+r4ravikumar-dev@users.noreply.github.com>
2023-08-18 11:29:23 +0000

    discussed about conversion and operations

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -23,3 +23,29 @@ let somenumber = 33
 let stringnumber = String (somenumber)
 console.log(stringnumber);
 console.log (typeof stringnumber);
+
+// ******************** Operations *****************
+
+let value =3
+let negvalue =  -value
+console.log(negvalue);
+
+let str1 = "hello"
+let str2 = " ravi"
+str3=str1+str2;
+console.log(str3);
+
+// in type conversion of datatypes in Javascript if string comes first then the numbers are being converted into string and then string operration takes place and if the numbers comes first then the operation takes place and then string operation takes place
+
+// some tricky conversion
+
+console.log(true);
+console.log(+true); // gives  1 as output
+
+let gamecounter =100
+gamecounter++; //postfix operation
+console.log(gamecounter);
+
+let gamecounter11 =100
+gamecounter1++; //prefix operation
+console.log(gamecounter1);
