de7d5144bd2b4f110fa9074014ef2aa1036959cf
Ravi Kumar <62147216+r4ravikumar-dev@users.noreply.github.com>
2023-08-18 11:33:16 +0000

    some extra notes were added in variable.js

--- basics/variables.js
+++ basics/variables.js
@@ -6,5 +6,7 @@ let accountState;
 
 // accountId = 2 // not allowed
 
-console.log("accountId");
-console.table([accountId, accountEmail, accoundPassword, accountCity, accountState])
+console.log("accountId"); // for printing singlel item
+console.table([accountId, accountEmail, accoundPassword, accountCity, accountState]) // for printing item in tabular form
+
+//prefer not to use var because of issue in block scope and functional scope so for variable deceleration use let
