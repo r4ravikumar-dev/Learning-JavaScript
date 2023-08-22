ad840f4c4e2b1871363b626eb3aacfa3407766ef
Ravi Kumar <62147216+r4ravikumar-dev@users.noreply.github.com>
2023-08-22 08:21:33 +0000

    changes done

--- Basics/basics_Part-4/02_scope.js
+++ Basics/basics_Part-4/02_scope.js
@@ -1,23 +1,23 @@
-// let a = 10
-// const b = 20
-// var c = 30 // this is termed as global scope and the value we declare here are usable by anywhere
+let a = 10
+const b = 20
+var c = 30 // this is termed as global scope and the value we declare here are usable by anywhere
 
 
-// {} // known as scope
+{} // known as scope
 
 
-// if (true){
-//     let a = 10
-//     const b = 20
-//     var c = 50 
-//     console.log("Inner:", a,b,c);
-// } //this is called as block scope and the value we decleare is limited only to this block
+if (true){
+    let a = 10
+    const b = 20
+    var c = 50 
+    console.log("Inner:", a,b,c);
+} //this is called as block scope and the value we decleare is limited only to this block
 
-// console.log(a);
-// console.log(b);
-// console.log(c); // in this line the output is the latest value of var what we assigned to it
+console.log(a);
+console.log(b);
+console.log(c); // in this line the output is the latest value of var what we assigned to it
 
-// // whether we use var as global or block scope it is usebalbe at anywhere
+// whether we use var as global or block scope it is usebalbe at anywhere
 
 
 //Nested Scope
