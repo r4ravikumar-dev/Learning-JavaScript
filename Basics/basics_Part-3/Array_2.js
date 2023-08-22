5ca3dcba428b4162ab9334dfd62036ece07921c0
Ravi Kumar <62147216+r4ravikumar-dev@users.noreply.github.com>
2023-08-22 06:29:40 +0000

    added some methods in array

--- Basics/basics_Part-3/Array_2.js
+++ Basics/basics_Part-3/Array_2.js
@@ -32,4 +32,52 @@ let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3)); //Returns a new array from a set of elements.
+console.log(Array.of(score1, score2, score3)); //Returns a new array from a set of elements. 
+
+
+
+//Some important methods of Array
+
+// at: ƒ at()
+// concat: ƒ concat()
+// constructor: ƒ Array()
+// copyWithin: ƒ copyWithin()
+// entries: ƒ entries()
+// every: ƒ every()
+// fill: ƒ fill()
+// filter: ƒ filter()
+// find: ƒ find()
+// findIndex: ƒ findIndex()
+// findLast: ƒ findLast()
+// findLastIndex:  ƒ findLastIndex()
+// flat: ƒ flat()
+// flatMap: ƒ flatMap()
+// forEach: ƒ forEach()
+// includes: ƒ includes()
+// indexOf: ƒ indexOf()
+// join: ƒ join()
+// keys: ƒ keys()
+// lastIndexOf: ƒ lastIndexOf()
+// length:0
+// map: ƒ map()
+// pop: ƒ pop()
+// push: ƒ push()
+// reduce: ƒ reduce()
+// reduceRight: ƒ reduceRight()
+// reverse: ƒ reverse()
+// shift: ƒ shift()
+// slice: ƒ slice()
+// some: ƒ some()
+// sort: ƒ sort()
+// splice: ƒ splice()
+// toLocaleString: ƒ toLocaleString()
+// toReversed: ƒ toReversed()
+// toSorted: ƒ toSorted()
+// toSpliced: ƒ toSpliced()
+// toString: ƒ toString()
+// unshift: ƒ unshift()
+// values: ƒ values()
+// with: ƒ with()
+// Symbol(Symbol.iterator): ƒ values()
+// Symbol(Symbol.unscopables): {at: true, copyWithin: true, entries: true, fill: true, find: true, …}
+// [[Prototype]]: Object
