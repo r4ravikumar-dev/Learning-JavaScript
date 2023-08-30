0cb46c9a561ad2bf8170802aace69cc62582e99d
Ravi Kumar <rravikumar27316@gmail.com>
2023-08-30 12:17:26 +0530

    discussed about basic loop concepts

---
+++ 03_loops/02_doWhileLoop.js
@@ -0,0 +1,22 @@
+
+// while loop //
+
+
+let index = 0;
+while (index <= 10)
+{
+    console.log(`Value of inddex is ${index}`);
+    index = index+2
+}
+
+// do while loop //
+
+let score = 1
+do{
+    console.log(`Score is ${score}`);
+    score++
+}
+while (score <= 10);
+{
+    console.log("Code is correct");
+}
