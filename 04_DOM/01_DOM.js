40b08920aacbf5e8613d8fde4c836c7b36e35191
Ravi Kumar <rravikumar27316@gmail.com>
2023-09-24 22:17:22 +0530

    learned basics of DOM

---
+++ 04_DOM/01_DOM.js
@@ -0,0 +1,5 @@
+console.log(window.document) // return all informationn of that document like function, method and everything of the document
+
+console.log(documnent); // returns only HTML related informationn of the document
+
+cconsole.dir(document); // return all the information of that document like console.log(window.document) method
