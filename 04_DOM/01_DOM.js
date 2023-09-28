8aa1ec5476bca9c22d1c083317df4d90c6caabe4
Ravi Kumar <rravikumar27316@gmail.com>
2023-09-28 21:52:02 +0530

    DOM second leacture

--- 04_DOM/01_DOM.js
+++ 04_DOM/01_DOM.js
@@ -3,3 +3,29 @@ console.log(window.document) // return all informationn of that document like fu
 console.log(documnent); // returns only HTML related informationn of the document
 
 cconsole.dir(document); // return all the information of that document like console.log(window.document) method
+
+
+
+//DOM selector NodeList and HTML collection
+
+document //to access the complete document
+
+document.getElementById('') // to get access of that particular Id
+
+document.getElementById('title').id //to return the name of that id
+
+document.getElementById('title').className //return the name of that calss
+
+document.getElementById('title').getAttribute('id') //Returns element's first attribute whose qualified name is qualifiedName, and null if there is no such attribute otherwise.
+
+document.getElementById('title').setAttribute('class', 'test heading') //Sets the value of element's first attribute whose qualified name is qualifiedName to value.
+
+
+
+const title = document.getElementById('id') //stores the element in the variable named title
+
+title.textContent //get the text of that element
+
+title.innerHTML // get the text of that element
+
+title.innerText //get the text of that element
