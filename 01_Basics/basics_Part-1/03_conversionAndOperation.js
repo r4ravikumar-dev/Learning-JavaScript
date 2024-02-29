7de82b4d5c67c30e73b166b3f29fe6ed6d76bdec
Ravi Kumar <rravikumar27316@gmail.com>
2024-02-29 20:43:57 +0530

    changes made in the 03 file

--- 01_Basics/basics_Part-1/03_conversionAndOperation.js
+++ 01_Basics/basics_Part-1/03_conversionAndOperation.js
@@ -3,12 +3,14 @@ let score = "33"
 console.log(typeof score); 
 console.log(typeof(score));
 
-let valueInNumber = Number(score)
+let valueInNumber = Number(score) //the value is being converted to a number from string value
 console.log(typeof valueInNumber)
 
 // "33" => 33
-// "33abc" => NaN
-// true => 1; false =>0
+// "33abc" => NaN aka not a number and the typer of NaN is number
+//"Null" => 0
+//"undefined" => NaN
+// true => 1; false => 0;
 
 // Boolean conversion
 let isLoggedIn = 1
@@ -20,7 +22,7 @@ console.log (booleanIsLoggedIn);
 // "ravi" => true
 
 let somenumber = 33
-let stringnumber = String (somenumber)
+let stringnumber = String (somenumber) // chasanged the value from number to string
 console.log(stringnumber);
 console.log (typeof stringnumber);
 
@@ -35,6 +37,9 @@ let str2 = " ravi"
 str3=str1+str2;
 console.log(str3);
 
+console.log("1" + 2); // output: 12
+console.log("2" + 2 + 2); // output:222
+console.log(2+2+"1"); // output:41
 // in type conversion of datatypes in Javascript if string comes first then the numbers are being converted into string and then string operration takes place and if the numbers comes first then the operation takes place and then string operation takes place
 
 // some tricky conversion
@@ -47,5 +52,5 @@ gamecounter++; //postfix operation
 console.log(gamecounter);
 
 let gamecounter11 =100
-gamecounter1++; //prefix operation
-console.log(gamecounter1);
+++gamecounter11; //prefix operation
+console.log(gamecounter11);
