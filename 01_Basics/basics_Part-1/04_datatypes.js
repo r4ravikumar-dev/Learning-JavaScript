45c598700616ad53cf59c2babb330c393054e821
Ravi Kumar <rravikumar27316@gmail.com>
2024-02-29 20:27:41 +0530

    changed some comments in the datatypes.js file

--- 01_Basics/basics_Part-1/04_datatypes.js
+++ 01_Basics/basics_Part-1/04_datatypes.js
@@ -1,18 +1,20 @@
 "use strict"; // treat all JS code as newer version
 
-// alert("hello") // we are using node JS not browser
+// alert("hello") 
+// we are using node JS not browser, the alert is used for sending the alert message inn the browser
 
-//number => 2 to power 53
-//bigint
-//string => ""
-//boolean => true/false
-//null =>standalone value
-//undefined
-//symbol => used for uniqueness
+/*Premetive datatypes*/
+// number => range of the number is till 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null =>standalone value and it is a representation of empty value
+// undefined => value till not defined
+// symbol => used for uniqueness
 
 //object
 
-//typeof => getting the type of variables
+//typeof => used for getting the type of variables
 
 console.log(typeof undefined); //undefined
 console.log(typeof null); //object
